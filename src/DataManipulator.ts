64c9e3b6fcf13fe756119c7af6e0f147a51f35b5
Dylan Buchi <devybuchi@gmail.com>
2022-10-23 14:13:33 -0300

    Complete Task 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,19 +1,47 @@
 import { ServerRespond } from './DataStreamer';
+import { calculateAverage } from './utilities/functions';
 
 export interface Row {
-  stock: string;
-  top_ask_price: number;
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
   timestamp: Date;
 }
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: (el.top_ask && el.top_ask.price) || 0,
-        timestamp: el.timestamp,
-      };
-    });
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = calculateAverage([
+      serverResponds[0].top_ask.price,
+      serverResponds[0].top_bid.price,
+    ]);
+
+    const priceDEF = calculateAverage([
+      serverResponds[1].top_ask.price,
+      serverResponds[1].top_bid.price,
+    ]);
+
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.02;
+    const lowerBound = 1 - 0.02;
+
+    const timestamp =
+      serverResponds[0].timestamp > serverResponds[1].timestamp
+        ? serverResponds[0].timestamp
+        : serverResponds[1].timestamp;
+
+    const triggerAlert =
+      ratio > upperBound || ratio < lowerBound ? ratio : undefined;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: triggerAlert,
+      ratio,
+      timestamp,
+    };
   }
 }
